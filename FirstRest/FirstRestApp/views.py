f65a89148d638a4d9e4f49146ef3202406158796
jawaher-alqotym <jwahralqtym@gmail.com>
2022-06-06 12:19:25 +0300

    first commit

--- FirstRest/FirstRestApp/views.py
+++ FirstRest/FirstRestApp/views.py
@@ -4,13 +4,13 @@ from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
-from datetime import date
+from datetime import date as my_date
 from random import randint
 
 @api_view(['GET'])
 def date(request : Request):
     data = {
-        "date" : f"Today is {date.today()}"
+        "date" : f"Today is {my_date.today()}"
     }
 
     return Response(data)
@@ -20,13 +20,15 @@ def random(request: Request):
 
     min =  request.data.get("min", None)
     max = request.data.get('max', None)
-
     random = randint(min, max)
 
-    data = {
-        "random" : random
-    }
+    if min < 0 :
+        data = {
+            "msg": "Not Allowed. Please provide a min that is bigger than 0"}
+        return Response(data, status=status.HTTP_400_BAD_REQUEST)
 
-
-    return Response(data, status=status.HTTP_200_OK)
+    else:
+        data = {
+        "random" : random }
+        return Response(data, status=status.HTTP_200_OK)
 
