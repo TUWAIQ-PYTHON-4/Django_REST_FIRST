a5d70bc6336d60c5b701a82e94691dccce12348d
Aaboassoud <a.aboassoud@gmail.com>
2022-06-06 11:54:42 +0300

    updated codes

--- my_first_API/Api_App/views.py
+++ my_first_API/Api_App/views.py
@@ -19,16 +19,16 @@ def date(request: Request):
 def random(request: Request):
 
     min = request.data.get("min",None)
+    max = request.data.get("max",None)
+    random1 = randint(min, max)
 
+    
     if min < 0:
         data = {
         "msg" : "Not Allowed. Please provide a min that is bigger than 0"
         }
         return Response(data, status=status.HTTP_400_BAD_REQUEST)
 
-    max = request.data.get("max",None)
-    random1 = randint(min, max)
-    print(random1)
     data = {
         "Random numbers genrates" : str(min) + " to " + str(max),
         "Random number genrated" : random1
