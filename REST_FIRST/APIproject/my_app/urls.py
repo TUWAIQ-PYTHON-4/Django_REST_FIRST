ce10126762cb4ffdaab5c191e5323bd7c57b8fa4
Faisal othman <Faisalalsneed@gmail.com>
2022-06-06 14:56:06 +0300

    LAB

---
+++ REST_FIRST/APIproject/my_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("date/", views.date, name="date"),
+    path("random/", views.random, name="random")
+    ]
---
+++ REST_FIRST/APIproject/my_app/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
+import datetime
+from random import randint
+import json
+import random
+@api_view(['GET'])
+def date(request : Request):
+    data = {
+        "date" : datetime.date.today()
+    }
+    return Response(data)
+@api_view(['POST'])
+def random(request: Request):
+    min = request.data.get('min',None)
+    max = request.data.get("max",None)
+    random1 = randint(min, max)
+    if min < 0:
+        data = {
+        "msg" : "Not Allowed. Please provide a min that is bigger than 0"
+        }
+        return Response(data, status=status.HTTP_400_BAD_REQUEST)
+    max = request.data.get("max",None)
+    random1 = randint(min, max)
+    print(random1)
+    data = {
+        "Random numbers genrates" : str(min) + " to " + str(max),
+        "Random number genrated" : random1
+    }
+    return Response(data, status=status.HTTP_200_OK)
